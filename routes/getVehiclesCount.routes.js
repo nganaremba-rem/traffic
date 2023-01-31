66362b9d5a4e004b2cf2316aba31d86378f61ad9
ThokchomRoshanSingh <thokchomroshan64@gmail.com>
2023-01-31 07:18:08 +0530

    Get cooldown

--- routes/getVehiclesCount.routes.js
+++ routes/getVehiclesCount.routes.js
@@ -1,6 +1,13 @@
 const vehicleCountModel = require("../model/vehicleCountModel");
 const router = require("express").Router();
 
+const ids = {
+  0: "light_1",
+  1: "light_2",
+  2: "light_3",
+  3: "light_4"
+}
+
 router.get("/", async (req, res) => {
   try {
     const response = await vehicleCountModel.find();
@@ -12,16 +19,28 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/current", async (req, res) => {
+router.get("/:id", async (req, res) => {
+  const id = req.params.id
   try {
     const response = await vehicleCountModel.find();
-    return res.send(response[response.length - 1]);
+    return res.send(response[response.length - 1][ids[id]]);
   } catch (err) {
     return res.status(500).json({
       message: err.message,
     });
   }
 });
+
+// router.get("/current", async (req, res) => {
+//   try {
+//     const response = await vehicleCountModel.find();
+//     return res.send(response[response.length - 1]);
+//   } catch (err) {
+//     return res.status(500).json({
+//       message: err.message,
+//     });
+//   }
+// });
 
 
 module.exports = router;
