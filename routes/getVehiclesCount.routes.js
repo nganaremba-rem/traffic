37c3dbf900cb34e40ec525ce5122cb5b8e2c5abc
ThokchomRoshanSingh <thokchomroshan64@gmail.com>
2023-01-31 07:23:38 +0530

    Get cooldown

--- routes/getVehiclesCount.routes.js
+++ routes/getVehiclesCount.routes.js
@@ -23,7 +23,7 @@ router.get("/:id", async (req, res) => {
   const id = req.params.id
   try {
     const response = await vehicleCountModel.find();
-    return res.send(response[response.length - 1][parseInt(ids[id])]);
+    return res.send(response[response.length - 1][ids[parseInt(id)]]);
   } catch (err) {
     return res.status(500).json({
       message: err.message,
